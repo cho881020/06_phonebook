5d5fde2eb48a01b7e03fe1a445f70289db5bd55a
조경진 <cho881020@gmail.com>
2022-10-21 14:49:31 +0900

    메뉴 선택값에 따른 분기
     - 0을 입력할때까지 메뉴 재출현

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 
 # 전화번호부 예시 문제
 
-print('===== 전화번호부 =====')
-print('1. 전화번호 등록')
-print('2. 저장된 전화번호 목록 조회')
-print('0. 프로그램 종료')
-print('=====================')
-input_num = int( input('메뉴 선택 : ') )
+# 사용자가 0을 넣을때 까지 반복
+# 무한히 입력 받다가, 0을 넣으면 종료
+
+while True:
+    print('===== 전화번호부 =====')
+    print('1. 전화번호 등록')
+    print('2. 저장된 전화번호 목록 조회')
+    print('0. 프로그램 종료')
+    print('=====================')
+    input_num = int( input('메뉴 선택 : ') )
+
+    if input_num == 0:
+        print('프로그램을 종료합니다.')
+        break
+    elif input_num == 1:
+        print('전화번호 추가를 선택했습니다.')
+    elif input_num == 2:
+        print('목록 조회를 선택했습니다.')
+    else:
+        print('잘못된 입력입니다.')
